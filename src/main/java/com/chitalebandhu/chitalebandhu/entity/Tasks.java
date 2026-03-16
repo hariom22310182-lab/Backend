4d963f76dfbaa6f73e6be79137b4d86fc3ec3d24
BlessForever04 <vinitwankhade411@gmail.com>
2026-03-16 23:17:22 +0530

    Minor change

--- src/main/java/com/chitalebandhu/chitalebandhu/entity/Tasks.java
+++ src/main/java/com/chitalebandhu/chitalebandhu/entity/Tasks.java
@@ -28,12 +28,6 @@ public class Tasks {
     private int remainingTask;
     private int completedTask;
 
-    // A task is a project if it has multiple sub-tasks
-    // A task is a task if it has no sub-tasks
-    // ,but we now have Type field in tasks entity so its no longer a confusion
-
-    // If we add sub-tasks in task, then that task is project
-
     public String getId() {
         return id;
     }
